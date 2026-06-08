414fc6753d0ac5d28211b8370e4137e38f1c0dc0
Mintae Kim <ibare77@gmail.com>
2026-06-08 21:47:35 +0400

    Initial Commit

---
+++ GitWatcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitWatcher
+//
+//  Created by Mintae Kim on 6/8/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitWatcher/GitWatcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitWatcherApp.swift
+//  GitWatcher
+//
+//  Created by Mintae Kim on 6/8/26.
+//
+
+import SwiftUI
+
+@main
+struct GitWatcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
